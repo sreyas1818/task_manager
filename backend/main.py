4763ffabee850e2f2df1e2ec057a2eecfdbdbe36
sreyas <sreyas593@gmail.com>
2026-07-03 13:27:18 +0530

    url change

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://task-manager-xbwv73qdf-sreyas1.vercel.app"
+        "task-manager-iota-pink.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
